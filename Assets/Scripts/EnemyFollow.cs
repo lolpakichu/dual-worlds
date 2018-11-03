d5df36b68477b081fff18ff0d5560828c92f21f9
Calvin Chiu <calvinchiu@Calvins-MacBook.local>
2018-11-03 18:29:52 -0500

    Patrolling AI for enemy

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour {
 
+    public float speed;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,25 @@ public class EnemyFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+
+        if(groundInfo.collider == false){
+            if(movingRight){
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            } else{
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+
+        }
 	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            Destroy(collision.gameObject);
+        }
+    }
 }
