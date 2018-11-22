bafb5e24bfd2fd570c2c00e4992f9e2d02b874d1
pakichu <smolguppy@gmail.com>
2018-11-22 10:05:34 -0600

    Menu Update

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+
+    public void ChangeScene(int nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+}
